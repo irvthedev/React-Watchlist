d818d8c2c303596a355d0a652a99ed542821c0fd
Irvin <irvthedev@gmail.com>
2022-04-09 13:43:24 -0700

    added watch list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { useEffect, useState } from 'react';
@@ -8,12 +8,15 @@ import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavorite from './components/AddFavorites';
 import RemoveFavourites from './components/RemoveFavourite';
+import AddMovieToWatch from './components/AddMovieToWatch';
+import RemoveWatchList from './components/RemoveMovieToWatch';
 
 
 const App = () => {
   const [movies,setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [favourites, setFavourites] = useState([]);
+  const [watchList, setWatchList] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
       const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=525001e1`;
@@ -33,7 +36,19 @@ const App = () => {
         localStorage.getItem('react-movie-app-favourites')
     );
 
-    setFavourites(movieFavourites);
+    if(movieFavourites != null)
+    {
+        setFavourites(movieFavourites);
+    }
+
+    const movieWatchList = JSON.parse(
+        localStorage.getItem('react-movie-app-watchlist')
+    );
+
+    if(movieWatchList != null)
+    {
+        setWatchList(movieWatchList);
+    }
 
   }, []);
 
@@ -42,15 +57,20 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
-  const saveToLocalStorage = (items) => {
+  const saveFavoriteToLocalStorage = (items) => {
     localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  };
+
+  const saveWatchListToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-watchlist', JSON.stringify(items))
   };
 
   // add movie to favourites
   const addFavouriteMovie = (movie) => {
-    const newFavouteList = [...favourites, movie]
-    setFavourites(newFavouteList)
-    saveToLocalStorage(newFavouteList);
+    
+        const newFavouteList = [...favourites, movie];
+        setFavourites(newFavouteList);
+        saveFavoriteToLocalStorage(newFavouteList);
   };
 
   // removing from favourites
@@ -58,9 +78,25 @@ const App = () => {
     // remove movie from list of favourites, compare ids
     const newFavouriteList = favourites.filter(
         (favourite) => favourite.imdbID !== movie.imdbID);
-    console.log(newFavouriteList);
     setFavourites(newFavouriteList);
-    saveToLocalStorage(newFavouriteList);
+    saveFavoriteToLocalStorage(newFavouriteList);
+  }
+
+  // add movie to favourites
+  const addWatchListMovie = (movie) => {
+    const newWatchList = [...watchList, movie];
+    setWatchList(newWatchList);
+    saveWatchListToLocalStorage(newWatchList);
+};
+
+ // removing from favourites
+ const removeWatchListMovie = (movie) => {
+     console.log("removing from watch");
+    // remove movie from list of favourites, compare ids
+    const newToWatchList = watchList.filter(
+        (watch) => watch.imdbID !== movie.imdbID);
+        setWatchList(newToWatchList);
+        saveWatchListToLocalStorage(newToWatchList);
   }
 
   return (
@@ -70,14 +106,35 @@ const App = () => {
             <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
         </div>
         <div className='row'>
-            <MovieList movies={movies} favoriteComponent={AddFavorite} handleFavouritesClick = {addFavouriteMovie}/>
+            <MovieList movies={movies} 
+                favoriteComponent={AddFavorite} 
+                handleFavouritesClick={addFavouriteMovie}
+                watchListComponent={AddMovieToWatch}
+                handleWatchListClick={addWatchListMovie}
+            />
+        </div>
+
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+            <MovieListHeading heading='Watch List'/>
+        </div>
+        <div className='row'>
+            <MovieList movies={watchList} 
+                favoriteComponent={AddFavorite} 
+                handleFavouritesClick={addFavouriteMovie}
+                watchListComponent={RemoveWatchList}
+                handleWatchListClick={removeWatchListMovie}
+            
+            />
         </div>
 
         <div className='row d-flex align-items-center mt-4 mb-4'>
             <MovieListHeading heading='Favourites'/>
         </div>
         <div className='row'>
-            <MovieList movies={favourites} favoriteComponent={RemoveFavourites} handleFavouritesClick = {removeFavouriteMovie}/>
+            <MovieList movies={favourites} favoriteComponent={RemoveFavourites} handleFavouritesClick = {removeFavouriteMovie}
+            watchListComponent={AddMovieToWatch}
+            handleWatchListClick={addWatchListMovie}
+            />
         </div>
 
     </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,19 +3,30 @@ import React from 'react';
 
 const MovieList = (props) => {
     const FavoriteComponent = props.favoriteComponent;
-    return (
-        <>
-            {props.movies.map((movie, index) => (
-                <div className='image-container d-flex justify-content-start m-3'>
-                    <img src={movie.Poster} alt='movie' ></img>
-                    <div onClick={ () => props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
-                        <FavoriteComponent/>
+    const WatchListComponent = props.watchListComponent;
+    if(props.movies.length > 0)
+    {
+        return (
+            <>
+                {props.movies.map((movie, index) => (
+                    <div className='image-container d-flex justify-content-start m-3'>
+                        <img src={movie.Poster} alt='movie' ></img>
+                        <div className='overlay d-flex'>
+                            <div onClick={ () => props.handleFavouritesClick(movie)} className='col'>
+                                <FavoriteComponent/>  
+                            </div>
+                            <div onClick={ () => props.handleWatchListClick(movie)} className='col'>
+                                <WatchListComponent/> 
+                            </div>
+                        </div>
+                        
                     </div>
-                </div>
-                
-            ))}
-        </>
-    )
+                    
+                ))}
+            </>
+        )
+    }
+    
 };
 
 export default MovieList;
